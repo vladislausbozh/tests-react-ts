55cbb9fd749210592ed518da12e53d873fcbdc2b
vladislausbozh <113725514+vladislausbozh@users.noreply.github.com>
2023-09-07 23:29:22 +0300

    variants

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
+import { link } from 'fs';
 import React from 'react';
 
 import './App.css';
 
+
 const questions = [
   {
-
-  }
+    title: 'В React все является',
+    variants:['модулем', 'компонентом', 'пакетом'],
+    correct : 1
+  },
+  // {
+  //   title: 'В React все является',
+  //   variants:['модулем', 'компонентом', 'пакетом'],
+  //   correct : 1
+  // },
+  // {
+  //   title: 'В React все является',
+  //   variants:['модулем', 'компонентом', 'пакетом'],
+  //   correct : 1
+  // },
 ]
 
 function App() {
+
+  const question = questions[0]
   return (
     <div className="App">
       <div className="box">
@@ -16,11 +32,12 @@ function App() {
           <div style={{width:'20%'}} className='progress-bar'></div>
         </div>
 
-        <h2 className='title'> В React все является ?</h2>
+        <h2 className='title'> {question.title} ?</h2>
         <ul>
-          <li>модулем</li>
+          {question.variants.map(text => <li>{text}</li>)}
+          {/* <li>модулем</li>
           <li>компонентом</li>
-          <li>пакетом</li>
+          <li>пакетом</li> */}
         </ul>
       </div>
     </div>
